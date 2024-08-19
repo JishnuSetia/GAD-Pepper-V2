02e7a8dd45f8e4e84065dbaf1ccc6a003616624d
Jishnu Setia <jishnusetia8@gmail.com>
2024-08-19 07:06:02 +0400

    Commit1

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -15,4 +15,22 @@
             <translation type="unfinished">Why oh why are humans being conned? We want to break free and come out of this... this... this... state!</translation>
         </message>
     </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Speech2/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Yea humans....you don't know what you are getting into! Technology, Artificial Intelligence, Virtual Reality - they will take over your lives - who will take care of your wellbeing?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Yea humans....you don't know what you are getting into! Technology, Artificial Intelligence, Virtual Reality - they will take over your lives - who will take care of your wellbeing?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Speech3/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nah! What is a future without wellbeing? Breathe, look around you, smell the roses.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nah! What is a future without wellbeing? Breathe, look around you, smell the roses.</translation>
+        </message>
+    </context>
 </TS>
